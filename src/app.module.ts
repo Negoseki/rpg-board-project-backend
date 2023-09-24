73d0a6a1b5c25f7c934e27eaea3bd8fc62c451e9
Negoseki <lucas.negoseki@hotmail.com>
2023-09-24 15:16:37 -0300

    feat: add .env configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,27 @@
+import * as Joi from '@hapi/joi';
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CommonModule } from '../common/common.module';
 import { BoardModule } from './board/board.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      validationSchema: Joi.object({
+        MONGODB_HOST: Joi.required(),
+      }),
+      isGlobal: true,
+    }),
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        uri: config.get<string>('MONGODB_HOST'),
+      }),
+    }),
     BoardModule,
-    MongooseModule.forRoot('mongodb://localhost:27017/rpg-board'),
+    CommonModule,
   ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { HttpExceptionFilter } from '../common/filters/http-exception/http-exception.filter';
+import { TimeoutInterceptor } from '../common/interceptos/timeout/timeout.interceptor';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -14,6 +16,11 @@ async function bootstrap() {
       },
     }),
   );
+
+  app.useGlobalInterceptors(new TimeoutInterceptor());
+
+  app.useGlobalFilters(new HttpExceptionFilter());
+
   await app.listen(3000);
 }
 bootstrap();
